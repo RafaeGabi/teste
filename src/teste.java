0050b675f32b757e33aaee65d6bab5673a279453
Rafael Rodrigues <rafaprodrigues1996@gmail.com>
2024-04-08 17:16:00 -0300

    teste

---
+++ src/teste.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class teste {
+    public static void main(String[] args) {
+        Scanner entrada;
+        entrada =  new Scanner (System.in);
+        int n1, n2;
+        n1 = entrada.nextInt();
+        n2 = entrada.nextInt();
+        int soma = n1 + n2;
+        System.out.println(soma);
+
+    }
+}
